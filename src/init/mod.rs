fe1bcd7ac1282494e3f922a619af4e21ae364e0f
Vo Van Nghia <git@vnghia.com>
2024-05-18 00:27:56 +0200

    fix(init): move generate zshenv to zsh mod

--- src/init/mod.rs
+++ src/init/mod.rs
@@ -1,10 +1,10 @@
-use std::io::Write;
-use std::path::{Path, PathBuf};
+mod zsh;
+
+use std::path::PathBuf;
 
 use clap::{Args, ValueEnum};
 use git2::Repository;
 use homedir::get_my_home;
-use itertools::Itertools;
 
 use crate::git::pull;
 
@@ -24,60 +24,6 @@ pub struct InitArgs {
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
 pub enum Shell {
     Zsh,
-}
-
-fn generate_zshenv<
-    P: AsRef<Path>,
-    PD: AsRef<Path>,
-    PC: AsRef<Path>,
-    PL: AsRef<Path>,
-    PB: AsRef<Path>,
->(
-    prefix: P,
-    dot_dir: PD,
-    code_dir: PC,
-    local_dir: PL,
-    bin_dir: PB,
-) {
-    let dot_dir = dot_dir.as_ref();
-    let code_dir = code_dir.as_ref();
-    let local_dir = local_dir.as_ref();
-    let bin_dir = bin_dir.as_ref();
-
-    let shell_dir = dot_dir.join("shell");
-    let sh_dir = shell_dir.join("common");
-    let zsh_dir = shell_dir.join("zsh");
-
-    let export_paths = [
-        ("DOTDIR", dot_dir),
-        ("CODEDIR", code_dir),
-        ("LOCALDIR", local_dir),
-        ("BINDIR", bin_dir),
-        ("SHDIR", &sh_dir),
-        ("ZDOTDIR", &zsh_dir),
-    ]
-    .into_iter()
-    .map(|(var, path)| format!("export {}={}", var, path.to_str().unwrap()))
-    .collect_vec();
-
-    let zshenv_path = prefix.as_ref().join(".zshenv");
-    let zshenv_content = [
-        ["# AUTO GENERATED FILE. DO NOT EDIT".to_string(), "".to_string()].as_slice(),
-        export_paths.as_slice(),
-        ["".to_string()].as_slice(),
-    ]
-    .concat()
-    .join("\n");
-    log::trace!(path:? = zshenv_path, content:? = zshenv_content; "Generating zshenv");
-
-    std::fs::File::options()
-        .write(true)
-        .create(true)
-        .truncate(true)
-        .open(&zshenv_path)
-        .expect("can not open .zshenv file to write")
-        .write_all(zshenv_content.as_bytes())
-        .expect("can not write to .zshenv file");
 }
 
 pub fn entry_init(args: InitArgs) {
@@ -102,7 +48,7 @@ pub fn entry_init(args: InitArgs) {
     }
 
     match args.shell {
-        Shell::Zsh => generate_zshenv(&prefix, &dot_dir, &code_dir, &local_dir, &bin_dir),
+        Shell::Zsh => zsh::generate_zshenv(&prefix, &dot_dir, &code_dir, &local_dir, &bin_dir),
     }
 
     std::fs::create_dir_all(&code_dir).expect("can not create code directory");
---
+++ src/init/zsh.rs
@@ -0,0 +1,58 @@
+use std::io::Write;
+use std::path::Path;
+
+use itertools::Itertools;
+
+pub fn generate_zshenv<
+    P: AsRef<Path>,
+    PD: AsRef<Path>,
+    PC: AsRef<Path>,
+    PL: AsRef<Path>,
+    PB: AsRef<Path>,
+>(
+    prefix: P,
+    dot_dir: PD,
+    code_dir: PC,
+    local_dir: PL,
+    bin_dir: PB,
+) {
+    let dot_dir = dot_dir.as_ref();
+    let code_dir = code_dir.as_ref();
+    let local_dir = local_dir.as_ref();
+    let bin_dir = bin_dir.as_ref();
+
+    let shell_dir = dot_dir.join("shell");
+    let sh_dir = shell_dir.join("common");
+    let zsh_dir = shell_dir.join("zsh");
+
+    let export_paths = [
+        ("DOTDIR", dot_dir),
+        ("CODEDIR", code_dir),
+        ("LOCALDIR", local_dir),
+        ("BINDIR", bin_dir),
+        ("SHDIR", &sh_dir),
+        ("ZDOTDIR", &zsh_dir),
+    ]
+    .into_iter()
+    .map(|(var, path)| format!("export {}={}", var, path.to_str().unwrap()))
+    .collect_vec();
+
+    let zshenv_path = prefix.as_ref().join(".zshenv");
+    let zshenv_content = [
+        ["# AUTO GENERATED FILE. DO NOT EDIT".to_string(), "".to_string()].as_slice(),
+        export_paths.as_slice(),
+        ["".to_string()].as_slice(),
+    ]
+    .concat()
+    .join("\n");
+    log::trace!(path:? = zshenv_path, content:? = zshenv_content; "Generating zshenv");
+
+    std::fs::File::options()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(&zshenv_path)
+        .expect("can not open .zshenv file to write")
+        .write_all(zshenv_content.as_bytes())
+        .expect("can not write to .zshenv file");
+}
