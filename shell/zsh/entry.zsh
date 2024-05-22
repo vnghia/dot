fe5b2cce63ce94c3d0a9ee3ca9c25d84726a3b49
Vo Van Nghia <git@vnghia.com>
2024-05-22 17:36:49 +0200

    feat(shell/zsh): add location histfile and compdump

--- shell/zsh/entry.zsh
+++ shell/zsh/entry.zsh
@@ -1,1 +1,9 @@
 source "$SHDIR/entry.sh"
+
+# History
+export HISTFILE="$LOCALDIR/.zsh_history"
+export HISTSIZE=999999999
+export SAVEHIST=999999999
+
+# Compdump
+export ZSH_COMPDUMP="$LOCALDIR/.zsh_compdump"
