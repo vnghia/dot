03032afed22f20455a1c7d6f3f84e3e312363626
Vo Van Nghia <git@vnghia.com>
2024-05-27 19:26:55 +0200

    feat(config): add config for git

--- shell/common/config.sh
+++ shell/common/config.sh
@@ -11,5 +11,8 @@ export RYE_HOME="$CONFIGDIR/rye"
 # Skm
 export SKM_STORE_PATH="$CONFIGDIR/skm"
 
+# Git
+export GIT_CONFIG_GLOBAL="$CONFIGDIR/git/.gitconfig"
+
 # Starship
 export STARSHIP_CONFIG="$CONFIGDIR/starship/starship.toml"
