0c77e5823ed6a33b3f19d2408928ffbf1b4b9f44
Vo Van Nghia <git@vnghia.com>
2024-06-01 23:34:58 +0200

    feat(shell/common): add color output for ls

--- shell/common/exit.sh
+++ shell/common/exit.sh
@@ -1,2 +1,8 @@
+# Ls colors
+export CLICOLOR=1
+export LSCOLORS="gxfxbxdxcxegedabagacad"
+export LS_COLORS="di=36:ln=35:so=31:pi=33:ex=32:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
+alias ls="ls --color=auto"
+
 # Local configuration that is specific to each machine.
 LOCAL_SH="$SHDIR/.local.sh" && test -f $LOCAL_SH && source $LOCAL_SH
