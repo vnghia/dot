86f87c77f8ed945484f5fb2ad2bb131804dd6390
Vo Van Nghia <git@vnghia.com>
2024-06-02 04:53:42 +0200

    feat(zsh): no dup in history file

--- shell/zsh/entry.zsh
+++ shell/zsh/entry.zsh
@@ -4,6 +4,12 @@ source "$SHDIR/entry.sh"
 export HISTFILE="$LOCALDIR/.zsh_history"
 export HISTSIZE=999999999
 export SAVEHIST=999999999
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_FIND_NO_DUPS
+setopt HIST_SAVE_NO_DUPS
 
 # Compdump
 export ZSH_COMPDUMP="$LOCALDIR/.zsh_compdump"
