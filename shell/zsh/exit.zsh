eaa2def3f8cf255dc0197316abae2e52644fc2b3
Vo Van Nghia <git@vnghia.com>
2024-08-07 13:55:57 +0200

    fix(zsh): allow to disablze zellij on the fly

--- shell/zsh/exit.zsh
+++ shell/zsh/exit.zsh
@@ -4,4 +4,5 @@ source "$SHDIR/exit.sh"
 LOCAL_ZSH="$ZDOTDIR/.local.zsh" && test -f $LOCAL_ZSH && source $LOCAL_ZSH
 
 # Start zellij after everything to avoid missing configuration.
-eval "$(zellij setup --generate-auto-start zsh)"
+# Don't start zellij if one of either two envs below is not empty.
+[[ -n $DISABLE_ZELLIJ || -n $VSCODE_INJECTION ]] || eval "$(zellij setup --generate-auto-start zsh)"
