4dfd70fe6cfa3e7633766cca665da9d27dd39641
Vo Van Nghia <git@vnghia.com>
2025-06-07 16:52:01 +0200

    config(zsh): only search history from the prefix

--- shell/zsh/interactive.zsh
+++ shell/zsh/interactive.zsh
@@ -15,6 +15,7 @@ source "$ZDOTDIR/plugins/config/zsh-syntax-highlighting.zsh"
 source "$ZDOTDIR/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 
 # Zsh history substring search must be sourced after syntax highlighting.
+source "$ZDOTDIR/plugins/config/zsh-history-substring-search.zsh"
 source "$ZDOTDIR/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh"
 bindkey "^[[A" history-substring-search-up
 bindkey "^[[B" history-substring-search-down
